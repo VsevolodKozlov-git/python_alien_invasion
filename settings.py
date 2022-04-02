f332b94d91115fc33c3eb97644e7682121b69aa1
VsevolodKozlov-git <71010414+VsevolodKozlov-git@users.noreply.github.com>
2022-04-02 13:21:26 +0300

    chapter 2 version

---
+++ settings.py
@@ -0,0 +1,27 @@
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings.
+        self.ship_speed_factor = 0.6
+
+        # Alien settings
+        self.fleet_speed_factor = 0.5
+        self.fleet_direction = 1
+        self.fleet_drop_distance = 15
+
+        # Bullet settings.
+        self.bullet_speed_factor = 5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 10
+
+        # game settings.
+        self.ship_limit = 3
